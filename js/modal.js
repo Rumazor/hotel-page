f32cd15a43f6f1c11b77ae1fed0e31b3b4800868
Jorge Matute <th3rum2084@gmail.com>
2021-11-02 12:00:39 -0500

    mejora en el modal

--- js/modal.js
+++ js/modal.js
@@ -5,24 +5,30 @@ $('.imagen-proyecto').on('click', function() {
 
     var rutaImagen = $(this).attr('src');
     var modal = '<div class="modal" id="modal"><img src="'+ rutaImagen +'" alt=""><div class="btn-cerrar" id="btnCerrar"><i class="fa fa-times" aria-hidden="true"></i></div></div>';
-
-
+    
+    
     $('#proyectos').after(modal);
 
     $('#btnCerrar').on('click', function(){
-        
-        $('#modal').remove();
+        $('#modal').fadeOut();
+    });
+
+    $('#modal').on('click', function(){
+        $('#modal').fadeOut();
     });
 
 });
 
+
+
 $(document).on('keyup', function(e){
 
     if(e.which==27) {
-        $('#modal').remove();
+        $('#modal').fadeOut();
     }
 
 })
+
 
 
 });
