357b9c00908add41479d385672dd24a91eab64be
Jorge Matute <th3rum2084@gmail.com>
2021-11-04 12:34:39 -0500

    slider listo

--- js/slider.js
+++ js/slider.js
@@ -10,24 +10,49 @@ $('#slider .slide:last').insertBefore('#slider .slide:first');
 
 //Margen negativo para que el primer slide se muestre
 
-slider.css('margin-left', '-29%');
+slider.css('margin-left', '-27%');
 
 // Funcion para que el slide se mueva a la derecha
 
 function moverDerecha() {
-    slider.animate({
-        marginLeft: '-84.7%'
-    }, 700, function(){
-        $('#slider .slide:first').insertAfter('#slider .slide:last');
-        slider.css('margin-left', '-32.7%')
-    });
+    if (!slider.is(':animated')) {
+        slider.animate({
+            marginLeft: '-82.7%'
+        }, 700, function(){
+            $('#slider .slide:first').insertAfter('#slider .slide:last');
+            slider.css('margin-left', '-30.7%')
+            resetInterval()
+        }); 
+    }
 }
 
 btnSiguiente.on('click', moverDerecha);
 
 
+function moverIzquierda() {
+    if (!slider.is(':animated')) {
+        $('#slider .slide:last').insertBefore('#slider .slide:first');
+    slider.css('margin-left', '-82.7%');
+    slider.animate({
+        marginLeft: '-30.7%'
+    }, 700, function(){
+        resetInterval();
+    });
+    }
+}
+
+
+btnAnterior.on('click', moverIzquierda);
 
 
 
+var intervalo = setInterval(moverDerecha, 3000);
+
+
+function resetInterval(){
+    clearInterval(intervalo);
+    intervalo = setInterval(moverDerecha, 3000);
+}
 
 });
+
