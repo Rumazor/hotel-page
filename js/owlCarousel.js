89817e39d7f48abc3f5293e5352e92ee24600eb8
Ruma <th3rum2@gmail.com>
2021-12-28 18:33:09 -0500

    carrusel añadido con jquery

---
+++ js/owlCarousel.js
@@ -0,0 +1,29 @@
+$(document).ready(function(){
+
+    var owl = $('.owl-carousel');
+    owl.owlCarousel({
+        items:3,
+        loop:true,
+        margin:20,
+        autoplay:true,
+        autoplayTimeout:2000,
+        autoplayHoverPause:true,
+        responsiveClass:true,
+        responsive:{
+            0:{
+                items:1
+            },
+            600:{
+                items:2
+            },1000:{
+                items:3
+            }
+        }
+    });
+    $('.play').on('click',function(){
+        owl.trigger('play.owl.autoplay',[1000])
+    })
+    $('.stop').on('click',function(){
+        owl.trigger('stop.owl.autoplay')
+    })
+})
